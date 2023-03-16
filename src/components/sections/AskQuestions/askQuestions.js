5b3664b8d826e67b0676faf92a161ab3f56266d9
Rajeev Kumar <rk.rajeevkr1503@gmail.com>
2023-03-16 13:39:27 +0530

    v2 new push

--- src/components/sections/AskQuestions/askQuestions.js
+++ src/components/sections/AskQuestions/askQuestions.js
@@ -2,22 +2,21 @@ import { TbSend } from "react-icons/tb";
 export default function AskQuestions(props) {
   return (
     <div
-      className="flex flex-col items-center gap-2 absolute border border-[#1a1a2e] bg-[#1a1a2e] right-2 top-[50px] rounded-lg w-[96%] md:w-[60%] p-0 md:p-4"
-      style={{ height: "90.5vh" }}
+      className="shadow-2xl flex flex-col items-center gap-2 absolute border border-[#1a1a2e] bg-[#1a1a2e] right-2 top-[50px] rounded-lg w-[96%] md:w-[60%] p-0 md:p-4"
+      style={{ height: "88vh" }}
     >
       <div className="h-[92%] w-full rounded-lg md:p-4 p-1 overflow-y-scroll scrollbarfeature">
-        <div className="flex flex-col gap-2 ">
+        <div className="flex flex-col gap-2">
           <div className="bg-gray-600 p-2 w-full rounded-lg flex flex-row md:gap-4 gap-2">
-            <div className="rounded-full text-md bg-[#13131e] p-1 px-2 h-min">Q</div>
-            <p>
+            <p className="leading-7 text-[16px] text-justify"><span className="rounded-full bg-[#13131e] p-1 px-2 h-min mr-4 text-xs">Question</span>
               Lorem Ipsum, Why do we use it? Lorem Ipsum, Why do we use it? Lorem
               Ipsum, Why do we use it? Lorem Ipsum, Why do we use it?
             </p>
           </div>
             <div className="p-2 w-full rounded-lg">
             
-              <p className="leading-7 text-[16px] text-justify"><span className="rounded-full bg-[#13131e] p-1 px-2 h-min mr-4">
-              A
+              <p className="leading-7 text-[16px] text-justify"><span className="rounded-full bg-[#13131e] p-1 px-2 h-min mr-4 text-xs">
+              Answer
             </span>
                 It is a long established fact that a reader will be distracted
                 by the readable content of a page when looking at its layout.
