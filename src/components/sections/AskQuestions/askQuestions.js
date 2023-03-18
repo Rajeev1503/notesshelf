5f320c6fd6b996efb8d70721fca34a37cfde83d0
Rajeev Kumar <rk.rajeevkr1503@gmail.com>
2023-03-18 17:18:18 +0530

    askquestion v3

--- src/components/sections/AskQuestions/askQuestions.js
+++ src/components/sections/AskQuestions/askQuestions.js
@@ -98,11 +98,11 @@ export default function AskQuestions(props) {
 
   return (
     <div
-      className={`h-[87vh] shadow-2xl flex flex-col items-center gap-2 absolute border border-[#1a1a2e] bg-[#1a1a2e] right-2 top-[50px] rounded-lg w-[99%] ${
+      className={`h-[87vh] shadow-2xl flex flex-col items-center gap-2 absolute border border-[#1a1a2e] bg-[#1a1a2e] right-1 md:right-2 top-[50px] rounded-lg w-[98%] ${
         maximiseWindow ? "md:w-96% md:h-[90vh]" : "md:w-[60%]"
       }`}
     >
-      <div className="px-4 border-b border-gray-800 w-full p-1 flex flex-row justify-end gap-2 text-gray-600">
+      <div className="px-4 border-b border-gray-800 w-full p-1 hidden md:flex flex-row justify-end gap-2 text-gray-600">
         {!maximiseWindow ? (
           <FaWindowMaximize onClick={() => setMaximiseWindow(true)} />
         ) : (
@@ -111,7 +111,7 @@ export default function AskQuestions(props) {
         <FaWindowClose onClick={props.askQuestionDisplay} />
       </div>
       <div className="md:p-4 p-2 py-4 w-full flex flex-col items-center gap-2 h-full">
-        <div className="h-[92%] w-full rounded-lg md:p-4 p-1 overflow-y-scroll scrollbarfeature">
+        <div className="md:h-[82%] h-[86%] w-full rounded-lg md:p-4 p-1 overflow-y-scroll scrollbarfeature">
           <div className="flex flex-col gap-2">
             <div className="bg-gray-600 p-2 w-full rounded-lg flex flex-row md:gap-4 gap-2">
               <div className="leading-7 text-[16px] text-justify flex flex-row gap-4 items-start">
@@ -122,8 +122,8 @@ export default function AskQuestions(props) {
               </div>
             </div>
             <div className="p-2 w-full rounded-lg">
-              <div className="leading-7 text-[16px] text-justify flex flex-row gap-4 items-start">
-                <div className="rounded-full bg-[#13131e] p-1 px-2 text-xs mt-1">
+              <div className="leading-7 text-[16px] text-justify">
+                <div className="rounded-full bg-[#13131e] p-1 px-2 text-xs mt-1 w-min">
                   Answer
                 </div>
                 {(answerLoading ==='loading') ?
